ad56d92eeb42e3dd68dad1b2ae72704cc006ec4d
iheb dridi <33296124+hbhb0@users.noreply.github.com>
2020-01-06 01:18:26 -0800

    Update environment.prod.ts
    PS : Don't put yr personal info to the public ever again 'tard.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyD-EPO4xbLihUb0R_DTupKiHNpUAiuZxR8",
-    authDomain: "e-commerce-85c4d.firebaseapp.com",
-    databaseURL: "https://e-commerce-85c4d.firebaseio.com",
-    projectId: "e-commerce-85c4d",
-    storageBucket: "e-commerce-85c4d.appspot.com",
-    messagingSenderId: "580917659095",
-    appId: "1:580917659095:web:27edf0f3b8d0f84d433792",
-    measurementId: "G-6ZECZM2LTG"
+    apiKey: "XXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXX"
   }
 };
